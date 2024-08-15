7973166f530d5fc1d8cbebc729e3d6bd5825c6c1
Aiden Fong <afong413@gmail.com>
2024-08-14 22:54:53 -0700

    Basic site layout.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Carlito", ...defaultTheme.fontFamily.sans],
+        serif: ["Crimson Text", ...defaultTheme.fontFamily.serif],
+        mono: ["Fira Sans Variable", ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 }
